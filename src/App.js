8e7a9d1b3b3e869c9f817e516104f4653a41bd89
Ashutosh-360 <ashutoshnaik360@gmail.com>
2022-10-08 16:35:24 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,73 +1,110 @@
-
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [item,setItem]=useState({
-    title:"",
-    quantity:"",
-    price:"",
+  const [item, setItem] = useState({
+    title: "",
+    quantity: "",
+    price: "",
   });
-  const [itemList,setItemList]=useState([]);
-  const [display,setDisplay]=useState(false);
-  const [displayAddItem,setDisplayAddItem]=useState(true);
+  const [itemList, setItemList] = useState([]);
+  const [display, setDisplay] = useState(false);
+  const [displayAddItem, setDisplayAddItem] = useState(true);
 
-
-  const onChangeHandler=(event)=>{
+  const onChangeHandler = (event) => {
     setItem({
-      ...item,[event.target.name]:event.target.value
-    })
-  }
+      ...item,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-  const onDisplayHandler=()=>{
+  const onDisplayHandler = () => {
     setDisplay(!display);
-  }
-  
+  };
 
-  const onSubmitHandler=()=>{
-     setItemList([...itemList,{
-      title:item.title,
-    quantity:item.quantity,
-    price:item.price,
-    }])
-  }
-
-  const addItemDisplay=()=>{
+  const onSubmitHandler = () => {
+    setItemList([
+      ...itemList,
+      {
+        title: item.title,
+        quantity: item.quantity,
+        price: item.price,
+      },
+    ]);
     setDisplayAddItem(!displayAddItem);
     setDisplay(!display);
+  };
 
-  }
+  const addItemDisplay = () => {
+    setDisplayAddItem(!displayAddItem);
+    setDisplay(!display);
+  };
   console.log(itemList);
   return (
-    <>
-    <h1>Personal Shopping List</h1>
-    {displayAddItem && <button onClick={addItemDisplay}>Add Item</button>}
-    
-    {display && <><h2>Add Item</h2>
-    <label htmlFor="title">Title</label>
-    <input onChange={onChangeHandler} value={item.title} type="text" name='title' placeholder='Insert Title' />
-    <label htmlFor="quantity">Quantity</label>
-    <input onChange={onChangeHandler} value={item.quantity} type="number" name='quantity' placeholder='0' />
-    <label htmlFor="price">Price</label>
-    <input onChange={onChangeHandler} value={item.price} type="number" name='price' placeholder='0.00' />
-    <button type='submit' onClick={onSubmitHandler}>Submit</button>
-    <button onClick={addItemDisplay}>Close</button></>
-    }
+    <div className="App">
+      
 
-    <h2>Groceries List</h2>
-      <ul>
-        {itemList.map((item,index)=>{
-            return <li key={index}>
-            <span>{item.title}</span>
-            <span>{item.quantity}</span>
-            <span>{item.price}</span>
-            </li>
-        })}
-      </ul>
+      {display && (
+        <div className="add-item-container">
+          <h1>Personal Shopping List</h1>
 
-    
+          <h2>Add Item</h2>
+          <label htmlFor="title">Title</label>
+          <input
+            onChange={onChangeHandler}
+            value={item.title}
+            type="text"
+            name="title"
+            placeholder="Insert Title"
+          />
+          <label htmlFor="quantity">Quantity</label>
+          <input
+            onChange={onChangeHandler}
+            value={item.quantity}
+            type="number"
+            name="quantity"
+            placeholder="0"
+          />
+          <label htmlFor="price">Price</label>
+          <input
+            onChange={onChangeHandler}
+            value={item.price}
+            type="number"
+            name="price"
+            placeholder="0.00"
+          />
+          <div className="btn-container">
+            <button type="submit" onClick={onSubmitHandler}>
+              Submit
+            </button>
+            <button onClick={addItemDisplay}>Close</button>
+          </div>
+        </div>
+      )}
 
-    </>
+      {displayAddItem && (
+        <div className="groceries-container">
+          <h1>Personal Shopping List</h1>
+          <h2>Groceries List</h2>
+          <ul>
+            {/* {itemList.map((item, index) => {
+              return (
+                <li key={index}>
+                  <span>{item.title}</span>
+                  <span>{item.quantity}</span>
+                  <span>{item.price}</span>
+                </li>
+              );
+            })} */}
+            <li>sdfbds</li>
+            <li>hsdbcds</li>
+          </ul>
+          {displayAddItem && 
+          <div className="additembtn"><button  onClick={addItemDisplay}>Add Item</button></div>}
+        </div>
+      )}
+      
+    </div>
   );
 }
 
